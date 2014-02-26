58ea11bfddb733e170bdefd79eef9d68068af77c
Tuukka Pasanen <pasanen.tuukka@gmail.com>
2014-02-26 19:00:47 +0200

    Added some examples that works with libsndfile

---
+++ libsndfile_port.c
@@ -0,0 +1,154 @@
+/*
+ * Most of the code is taken from these tutorials
+ * https://www.assembla.com/code/portaudio/subversion/nodes/1922/portaudio/trunk/examples/paex_sine.c
+ * And added audio playing with libsndfile (FLAC, WAV, AIFF)
+ *
+ * You need:
+ * Portaudio development file (headers and libraries) http://www.portaudio.com
+ * Libsnfile development file (headers and libraries) http://www.mega-nerd.com/libsndfile/ at least version 1.0.25
+ *
+ * Compile with
+ * gcc -g $(pkg-config --cflags --libs portaudio-2.0) -lm -lsndfile libsndfile_port.c -o libsndfile_port
+ *
+ * Run with ./libsndfile_port some.wav/.flac/.aiff
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <portaudio.h>
+#include <sndfile.h>
+#include <signal.h>
+
+SNDFILE *infile;
+SF_INFO sfinfo ;
+
+/* Reques for writing length data */
+static int paLibsndfileCb( const void *inputBuffer, void *outputBuffer,
+                           unsigned long framesPerBuffer,
+                           const PaStreamCallbackTimeInfo* timeInfo,
+                           PaStreamCallbackFlags statusFlags,
+                           void *userData ) {
+    float *out = (float*)outputBuffer;
+    long readcount = 0;
+
+    /* Just empty buffer for no reason.. fun yea */
+    memset( out, 0x00, (framesPerBuffer * 2) * 4);
+
+    /* Read with libsndfile */
+    readcount = sf_read_float(infile, out, framesPerBuffer * 2);
+
+    /* File end if we read -1 */
+    if( readcount <= 0 ) {
+        printf("File end!\n");
+        return -1;
+    }
+
+    return paContinue;
+}
+
+/* Handle termination with CTRL-C */
+static void handler(int sig, siginfo_t *si, void *unused) {
+    printf("Got SIGSEGV at address: 0x%lx\n", (long) si->si_addr);
+}
+
+int main(int argc, char *argv[]) {
+    PaStreamParameters outputParameters;
+    PaStream *stream;
+    PaError retval = 0;
+    int r;
+    int pa_ready = 0;
+    unsigned int a;
+    double amp;
+    struct sigaction sa;
+
+    /* Open file. Because this is just a example we asume
+      What you are doing and give file first argument */
+    if (! (infile = sf_open(argv[1], SFM_READ, &sfinfo))) {
+        printf ("Not able to open input file %s.\n", "input.wav") ;
+        sf_perror (NULL) ;
+        return  1 ;
+    }
+
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = handler;
+
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        printf("Can't set SIGINT handler!\n");
+        sf_close(infile);
+        return -1;
+    }
+
+    if (sigaction(SIGHUP, &sa, NULL) == -1) {
+        printf("Can't set SIGHUP handler!\n");
+        sf_close(infile);
+        return -1;
+    }
+
+    retval = Pa_Initialize();
+
+    if( retval != paNoError ) {
+        goto exit;
+    }
+
+    outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
+
+    if (outputParameters.device == paNoDevice) {
+        fprintf(stderr, "Error: No default output device.\n");
+        goto exit;
+    }
+
+    outputParameters.channelCount = 2;       /* stereo output */
+    outputParameters.sampleFormat = paFloat32; /* 32 bit floating point output */
+    outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+    outputParameters.hostApiSpecificStreamInfo = NULL;
+
+    retval = Pa_OpenStream(
+                 &stream,
+                 NULL, /* no input */
+                 &outputParameters,
+                 44100,
+                 256,
+                 paClipOff,      /* we won't output out of range samples so don't bother clipping them */
+                 paLibsndfileCb,
+                 infile );
+
+    if( retval != paNoError ) {
+        goto exit;
+    }
+
+    retval = Pa_StartStream( stream );
+
+    if( retval != paNoError ) {
+        goto exit;
+    }
+
+    printf("Play maximum 180 seconds.\n");
+    Pa_Sleep( 1000 * 180 );
+
+    retval = Pa_StopStream( stream );
+
+    if( retval != paNoError ) {
+        goto exit;
+    }
+
+    retval = Pa_CloseStream( stream );
+
+    if( retval != paNoError ) {
+        goto exit;
+    }
+
+    Pa_Terminate();
+
+exit:
+    /* clean up and disconnect */
+    printf("\nExit and clean\n");
+    sf_close(infile);
+    Pa_Terminate();
+
+
+    return retval;
+}
+
+
---
+++ libsndfile_pulse.c
@@ -0,0 +1,334 @@
+
+
+//
+// Most of the code is taken from these tutorials
+// http://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/Developer/Clients/Samples/AsyncDeviceList/
+// http://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/Developer/Clients/Samples/AsyncPlayback/
+// And added audio playing with libsndfile (FLAC, WAV, AIFF)
+//
+// You need:
+// Pulseaudio development file (headers and libraries) http://www.freedesktop.org/wiki/Software/PulseAudio/ at least version 3.0
+// Libsnfile development file (headers and libraries) http://www.mega-nerd.com/libsndfile/ at least version 1.0.25
+//
+// Compile with
+// gcc -g $(pkg-config --cflags --libs libpulse) -lm -lsndfile libsndfile_pulse.c -o libsndfile_pulse
+//
+// Run with ./libsndfile_pulse some.wav
+//
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <pulse/pulseaudio.h>
+#include <sndfile.h>
+
+static int latency = 20000; // start latency in micro seconds
+void *sampledata = NULL;
+static pa_buffer_attr bufattr;
+static int underflows = 0;
+static pa_sample_spec ss;
+SNDFILE *infile;
+SF_INFO sfinfo ;
+int loop = 0;
+
+// When context change state this called
+void pa_state_cb(pa_context *c, void *userdata) {
+    pa_context_state_t state;
+    int *pa_ready = userdata;
+    state = pa_context_get_state(c);
+
+    switch  (state) {
+            // These are just here for reference
+        case PA_CONTEXT_UNCONNECTED:
+            printf("PA_CONTEXT_UNCONNECTED\n");
+            break;
+
+        case PA_CONTEXT_CONNECTING:
+            printf("PA_CONTEXT_CONNECTING\n");
+            break;
+
+        case PA_CONTEXT_AUTHORIZING:
+            printf("PA_CONTEXT_AUTHORIZING\n");
+            break;
+
+        case PA_CONTEXT_SETTING_NAME:
+            printf("PA_CONTEXT_NAME\n");
+            break;
+
+        default:
+            break;
+
+        case PA_CONTEXT_FAILED:
+            printf("PA_CONTEXT_FAILED\n");
+            *pa_ready = 2;
+            break;
+
+        case PA_CONTEXT_TERMINATED:
+            printf("PA_CONTEXT_TERMINATED\n");
+            *pa_ready = 2;
+            break;
+
+        case PA_CONTEXT_READY:
+            printf("PA_CONTEXT_READY\n");
+
+            *pa_ready = 1;
+            break;
+    }
+}
+
+// List sinks in our system (For playback)
+void pa_sinklist_cb(pa_context *c, const pa_sink_info *l, int eol, void *userdata) {
+    if( eol ) {
+        return;
+    }
+
+    printf("Sink Name: %s ", l->name);
+    printf("Description: %s\n", l->description);
+}
+
+// List sources in our system (For recording)
+void pa_sourcelist_cb(pa_context *c, const pa_source_info *l, int eol, void *userdata) {
+    if( eol ) {
+        return;
+    }
+
+    printf("Source Name: %s ", l->name);
+    printf("Description: %s\n", l->description);
+}
+
+// Anything happens call this
+static void stream_notify_cb(pa_stream *s, void *userdata) {
+    char sst[PA_SAMPLE_SPEC_SNPRINT_MAX];
+    char cmt[PA_SAMPLE_SPEC_SNPRINT_MAX];
+    printf("Using sample spec '%s', channel map '%s'.",
+           pa_sample_spec_snprint(sst, sizeof(sst), pa_stream_get_sample_spec(s)),
+           pa_channel_map_snprint(cmt, sizeof(cmt), pa_stream_get_channel_map(s)));
+
+    printf("Connected to device %s (index: %u, %ssuspended).\n",
+           pa_stream_get_device_name(s),
+           pa_stream_get_device_index(s),
+           pa_stream_is_suspended(s) ? "" : "not ");
+}
+
+// Reques for writing length data
+static void stream_request_cb(pa_stream *s, size_t length, void *userdata) {
+    pa_usec_t usec;
+    int neg;
+    int readcount = 0;
+
+    // Measure latency
+    pa_stream_get_latency(s, &usec, &neg);
+
+    // Allocate dynamically data
+    sampledata = pa_xmalloc(length * 2);
+    memset(sampledata, 0x00, length);
+    // Read with libsndfile
+    readcount = sf_read_short(infile, sampledata, length / 2);
+
+    // Print some statistics
+    printf("  latency %8d us request: %8d readed %8d\r", (int)usec, length, readcount);
+
+    // File end if we read -1
+    if( readcount <= 0 ) {
+        printf("File end!\n");
+        loop = 1;
+    }
+
+    // After that write to the Pulseaudio sink
+    if( pa_stream_write(s, (int *) sampledata, length, NULL, 0, PA_SEEK_RELATIVE) ) {
+        printf("Something wrong!\n");
+    }
+
+    // Free buffer
+    pa_xfree(sampledata);
+    sampledata = NULL;
+}
+
+// There is not enough bytes to flow so we call underflow
+static void stream_underflow_cb(pa_stream *s, void *userdata) {
+    // We increase the latency by 50% if we get 6 underflows and latency is under 2s
+    // This is very useful for over the network playback that can't handle low latencies
+    printf("underflow\n");
+    underflows++;
+
+    if (underflows >= 6 && latency < 2000000) {
+        latency = (latency * 3) / 2;
+        bufattr.maxlength = pa_usec_to_bytes(latency, &ss);
+        bufattr.tlength = pa_usec_to_bytes(latency, &ss);
+        pa_stream_set_buffer_attr(s, &bufattr, NULL, NULL);
+        underflows = 0;
+        printf("latency increased to %d\n", latency);
+    }
+}
+
+// Handle termination with CTRL-C
+static void handler(int sig, siginfo_t *si, void *unused) {
+    printf("Got SIGSEGV at address: 0x%lx\n", (long) si->si_addr);
+    loop = 1;
+}
+
+int main(int argc, char *argv[]) {
+    pa_mainloop *pa_ml;
+    pa_mainloop_api *pa_mlapi;
+    pa_context *pa_ctx;
+    pa_stream *playstream;
+    pa_operation *pa_op;
+    int r;
+    int pa_ready = 0;
+    int retval = 0;
+    unsigned int a;
+    double amp;
+    struct sigaction sa;
+
+    // Open file. Because this is just a example we asume
+    // What you are doing and give file first argument
+    if (! (infile = sf_open(argv[1], SFM_READ, &sfinfo))) {
+        printf ("Not able to open input file %s.\n", "input.wav") ;
+        sf_perror (NULL) ;
+        return  1 ;
+    }
+
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = handler;
+
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        printf("Can't set SIGINT handler!\n");
+        sf_close(infile);
+        return -1;
+    }
+
+    if (sigaction(SIGHUP, &sa, NULL) == -1) {
+        printf("Can't set SIGHUP handler!\n");
+        sf_close(infile);
+        return -1;
+    }
+
+    // Create a mainloop API and connection to the default server
+    pa_ml = pa_mainloop_new();
+    pa_mlapi = pa_mainloop_get_api(pa_ml);
+    pa_ctx = pa_context_new(pa_mlapi, "Simple PA test application");
+    pa_context_connect(pa_ctx, NULL, 0, NULL);
+
+    // Define what callback is called in state change
+    pa_context_set_state_callback(pa_ctx, pa_state_cb, &pa_ready);
+
+    // We can't do anything until PA is ready, so just iterate the mainloop
+    // and continue
+    while (pa_ready == 0) {
+        pa_mainloop_iterate(pa_ml, 1, NULL);
+    }
+
+    if (pa_ready == 2) {
+        retval = -1;
+        goto exit;
+    }
+
+    // Request sink infos
+    pa_op = pa_context_get_sink_info_list(pa_ctx, pa_sinklist_cb, NULL);
+
+    // We iterate and wait for PA_OPERATION_DONE
+    while(1) {
+        pa_mainloop_iterate(pa_ml, 1, NULL);
+
+        if (pa_operation_get_state(pa_op) == PA_OPERATION_DONE) {
+            pa_operation_unref(pa_op);
+            break;
+        }
+    }
+
+    // Request source infos
+    pa_op = pa_context_get_source_info_list(pa_ctx, pa_sourcelist_cb, NULL);
+
+    while(1) {
+        pa_mainloop_iterate(pa_ml, 1, NULL);
+
+        if (pa_operation_get_state(pa_op) == PA_OPERATION_DONE) {
+            pa_operation_unref(pa_op);
+            break;
+        }
+    }
+
+
+
+    printf("We have samplerate: %5d and channels %2d\n", sfinfo.samplerate, sfinfo.channels);
+
+    if( sfinfo.format & SF_FORMAT_PCM_S8 ) {
+        printf("Subformat: 8-bit!\n");
+
+    } else if( sfinfo.format & SF_FORMAT_PCM_16 ) {
+        printf("Subformat: 16-bit!\n");
+
+    } else if( sfinfo.format & SF_FORMAT_PCM_24 ) {
+        printf("Subformat: 24-bit!\n");
+
+    } else if( sfinfo.format & SF_FORMAT_PCM_32 ) {
+        printf("Subformat: 24-bit!\n");
+
+    } else if( sfinfo.format & SF_FORMAT_FLOAT ) {
+        printf("Subformat: FLOAT!\n");
+
+    } else if( sfinfo.format & SF_FORMAT_DOUBLE ) {
+        printf("Subformat: DOUBLE!\n");
+    }
+
+    ss.rate = sfinfo.samplerate;
+    ss.channels = sfinfo.channels;
+    ss.format = PA_SAMPLE_S16LE;
+
+    playstream = pa_stream_new(pa_ctx, "Playback", &ss, NULL);
+
+    if (!playstream) {
+        printf("pa_stream_new failed\n");
+    }
+
+    // Callback for writing
+    pa_stream_set_write_callback(playstream, stream_request_cb, NULL);
+    // Callback for underflow
+    pa_stream_set_underflow_callback(playstream, stream_underflow_cb, NULL);
+    // Stream has started
+    pa_stream_set_started_callback(playstream, stream_notify_cb, NULL);
+
+    bufattr.fragsize = (uint32_t) - 1;
+    bufattr.maxlength = pa_usec_to_bytes(latency, &ss);
+    bufattr.minreq = pa_usec_to_bytes(0, &ss);
+    bufattr.prebuf = (uint32_t) - 1;
+    bufattr.tlength = pa_usec_to_bytes(latency, &ss);
+
+    // Connect playback to default output
+    r = pa_stream_connect_playback(playstream, NULL, &bufattr,
+                                   PA_STREAM_INTERPOLATE_TIMING
+                                   | PA_STREAM_ADJUST_LATENCY
+                                   | PA_STREAM_AUTO_TIMING_UPDATE, NULL, NULL);
+
+    if (r < 0) {
+        // Old pulse audio servers don't like the ADJUST_LATENCY flag, so retry without that
+        r = pa_stream_connect_playback(playstream, NULL, &bufattr,
+                                       PA_STREAM_INTERPOLATE_TIMING |
+                                       PA_STREAM_AUTO_TIMING_UPDATE, NULL, NULL);
+    }
+
+    if (r < 0) {
+        printf("pa_stream_connect_playback failed\n");
+        retval = -1;
+        goto exit;
+    }
+
+    // Iterate the main loop and go again.  The second argument is whether
+    // or not the iteration should block until something is ready to be
+    // done.  Set it to zero for non-blocking.
+    while (!loop) {
+        pa_mainloop_iterate(pa_ml, 1, NULL);
+    }
+
+exit:
+    // clean up and disconnect
+    printf("\nExit and clean\n");
+    sf_close(infile);
+    pa_context_disconnect(pa_ctx);
+    pa_context_unref(pa_ctx);
+    pa_mainloop_free(pa_ml);
+    return retval;
+}
+
+
